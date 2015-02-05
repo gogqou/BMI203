339aabbbd148afd5c93d2cfe82e982d420fa4af9
Grace Ou <grace.ou@berkeley.edu>
2015-02-04 16:52:12 -0800

    messed up commit
    now has saved SW_run and mk_dict

---
+++ SmithWaterman/sub_matrix_mk_dict.py
@@ -0,0 +1,15 @@
+'''
+Created on Feb 4, 2015
+
+@author: gogqou
+
+reads in substitution matrix and gives back a dictionary of [a,b] keys that are associated with substitution values
+
+'''
+def submatrix_dict(inputFile):
+    
+    sub_dict = {}
+    
+    
+    
+    return sub_dict
