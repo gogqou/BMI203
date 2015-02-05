cf6fd1c9d82958ea47ce229b62a3a9c843752491
Grace Ou <grace.ou@berkeley.edu>
2015-02-04 16:15:26 -0800

    Initial startup of Smith Waterman. No code yet.

---
+++ SmithWaterman/main.py
@@ -0,0 +1,28 @@
+'''
+Created on Feb 4, 2015
+
+@author: gogqou
+
+Implements Smith Waterman Algorithm for local sequence alignment
+
+
+
+Implement the Smith-Waterman algorithm and instrument the code such
+that it can use any scoring matrix provided (i.e. will read it in from
+a separate file). 
+
+Scoring matrix includes gap-scoring scheme and substitution matrix; needs to be able to read in from outside file.
+
+
+
+Take input: seq1 and seq2
+Substitution matrix file--should call a parsing function to read in the file
+Gap scoring definitions
+
+Output: filled out similarity matrix H
+
+
+'''
+
+if __name__ == '__main__':
+    pass
