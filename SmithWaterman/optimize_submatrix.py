e9f362080b9e1ff4b6b7c4f7635907c3bfea296b
Grace Ou <grace.ou@berkeley.edu>
2015-02-08 19:05:15 -0800

    setting up structure to optimize by calculating new scores from count matrix multiplied by substitution matrix, then summing up those values

--- SmithWaterman/optimize_submatrix.py
+++ SmithWaterman/optimize_submatrix.py
@@ -32,15 +32,20 @@ def obj_function(pos_scores, neg_scores):
         obj_fnc_val = obj_fnc_val + TPrate/100
     return obj_fnc_val
 def calc_new_scores(pos_align_array,neg_align_array, subMatrix):
+    print len(pos_align_array)
     new_pos_scores=np.zeros(len(pos_align_array))
     new_neg_scores=np.zeros(len(neg_align_array))
+    for i in range(0,len(pos_align_array)):
+        new_pos_scores [i]= np.sum(pos_align_array[i]*subMatrix)
+    for i in range(0,len(neg_align_array)):
+        new_neg_scores [i]= np.sum(neg_align_array[i]*subMatrix)
     return new_pos_scores, new_neg_scores
 
 
 def generate_newsubMatrix(origsubMatrix):
     
     
-    
+    newsubMatrix = 0
     return newsubMatrix
 def main():
     if len(sys.argv)>5:
@@ -49,16 +54,29 @@ def main():
     pos_seq_list_name = sys.argv[1]
     neg_seq_list_name = sys.argv[2]
     home = sys.argv[3]
+    
     pos_seq_list_file = home+pos_seq_list_name+'.txt'
     neg_seq_list_file = home+neg_seq_list_name+'.txt'
+    
     subMatrixFile = sys.argv[4]
+    
     gap_init = 11
     gap_ext = 2
+    
     [sub_Matrixdict, origSubMatrix] = subMdict.mk_dict(home+subMatrixFile)
 
     [pos_scores, pos_align_array] = slSW.scores_from_seq_list(home, pos_seq_list_file, sub_Matrixdict, origSubMatrix, gap_init, gap_ext)
     [neg_scores, neg_align_array]= slSW.scores_from_seq_list(home, neg_seq_list_file, sub_Matrixdict, origSubMatrix, gap_init, gap_ext)
-
+    RWFile.writetxt(pos_align_array, home, 'pos_align_array.txt')
+    RWFile.writetxt(neg_align_array, home, 'neg_align_array.txt')
+    
+    temp=np.genfromtxt(home+'neg_align_array.txt', comments="#",  unpack=False)
+    print neg_align_array
+    print temp
+    [new_pos_scores, new_neg_scores] = calc_new_scores(pos_align_array, neg_align_array, origSubMatrix)
+    print new_pos_scores
+    
+    
     print 'done'
     
     return 1
