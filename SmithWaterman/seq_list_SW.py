93ae2f52e1b6817837783820479e0697620f8f91
Grace Ou <grace.ou@berkeley.edu>
2015-02-09 23:50:46 -0800

    [['BLOSUM50', 0.22], ['BLOSUM62', 0.22], ['MATIO', 0.36], ['PAM100',0.26], ['PAM250', 0.3]]

--- SmithWaterman/seq_list_SW.py
+++ SmithWaterman/seq_list_SW.py
@@ -90,13 +90,13 @@ def main():
     neg_seq_list_file = home+neg_seq_list_name+'.txt'
     subMatrixFile = sys.argv[4]
     subMatrixFile_list = ['BLOSUM50', 'BLOSUM62', 'MATIO', 'PAM100', 'PAM250']
-    FP_array = np.zeros([120,3])
-    #FP_array = []
+    #FP_array = np.zeros([120,3])
+    FP_array = []
     i=0
     #gap_init_cost, gap_ext_cost
     [sub_Matrix, origSubMatrix, AAlist] = subMdict.mk_dict(home+subMatrixFile)
     
-    
+    '''
     #for testing gap costs
     
     for gap_init in range(1,21):
@@ -119,14 +119,14 @@ def main():
     for i in range(0,len(subMatrixFile_list)):
         [sub_Matrix, origsubMatrix, AAlist] = subMdict.mk_dict(home+subMatrixFile_list[i])
     
-        [pos_score_list, pos_alignment_array] = scores_from_seq_list(home,pos_seq_list_file, sub_Matrix, origSubMatrix,13, 3)
-        [neg_score_list, neg_alignment_array] = scores_from_seq_list(home,neg_seq_list_file, sub_Matrix, origSubMatrix, 13, 3)
+        [pos_score_list, pos_alignment_array] = scores_from_seq_list(home,pos_seq_list_file, sub_Matrix, origSubMatrix,7, 3)
+        [neg_score_list, neg_alignment_array] = scores_from_seq_list(home,neg_seq_list_file, sub_Matrix, origSubMatrix, 7, 3)
         false_pos_rt= false_pos_rate(pos_score_list, neg_score_list)
         FP_array.append([subMatrixFile_list[i], false_pos_rt])
         
     print FP_array
          
-    '''      
+         
             
             
     print 'done'
